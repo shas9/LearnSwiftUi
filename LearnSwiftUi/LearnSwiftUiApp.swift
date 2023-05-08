25cb3b365bfddba21388d381d748fc4f1fccffbd
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-05-08 18:47:49 +0600

    Initial Commit

---
+++ LearnSwiftUi/LearnSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUiApp.swift
+//  LearnSwiftUi
+//
+//  Created by Shahwat Hasnaine on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
