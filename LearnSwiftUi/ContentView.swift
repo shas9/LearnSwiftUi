2063a5116e7ec1f0fd63f17b59f24edb687f7f2b
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-05-08 19:14:50 +0600

    The view protocol

--- LearnSwiftUi/ContentView.swift
+++ LearnSwiftUi/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                
+                Image(systemName: "swift")
+            }
+            Text("Hello yo, world!")
         }
         .padding()
     }
